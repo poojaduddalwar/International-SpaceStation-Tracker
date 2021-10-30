30a8e83c8fb48fae710c6accddef59bff3468fef
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-30 20:16:46 +0530

    dark mode and some css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,19 @@
+const LngElement = document.querySelector("#longitude")
+const LatElement = document.querySelector("#latitude")
+const VeloElement = document.querySelector("#velocity")
+const DarkButton = document.querySelector(".switch-class-button")
+const Body = document.querySelector("body")
+
+const darkmodefun = () => {
+    Body.classList.toggle('dark')
+}
+DarkButton.addEventListener('click',darkmodefun)
+
 var map = L.map('map').setView([0,0], 1);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
 var myIcon = L.icon({
     iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/International_Space_Station.svg/1200px-International_Space_Station.svg.png',
@@ -28,33 +41,12 @@ let marker = L.marker([0, 0], {icon: myIcon}).addTo(map)
 const fetchSpaceStationDetails = async()=>{
     const res = await fetch("https://api.wheretheiss.at/v1/satellites/25544")
     const data = await res.json()
-    const {latitude , longitude} = data
-    // console.log(latitude,longitude)
-    // console.log(data)
+    const {latitude , longitude , velocity} = data
+    LngElement.innerHTML = `Longitude : ${longitude.toFixed(2)} \u00B0`
+    LatElement.innerHTML = `Latitude : ${latitude.toFixed(2)} \u00B0`
+    VeloElement.innerHTML = `Velocity : ${velocity.toFixed(0)} `
     marker.setLatLng([latitude , longitude])
     L.marker([latitude,longitude],{icon:pathIcon}).addTo(map)
-    //setLatLng function changes the marker position to the given point
 }
 
-// To see live location -> call function after 1 sec
-// fetchSpaceStationDetails()
-
-//A function which calls particular function after set interval, here to give live location-
 setInterval(fetchSpaceStationDetails, 1000)
-
-// console.log(L)
-
-// var map = L.map('map').setView([20.9320, 77.7523], 1);
-//L.map is kind of initialize the function that initializes the leaflet map inside the element whose id you specify.
-//setview function sets the view
-//in setview the 2nd parameter is the initial zoom level which is going to show up on the load
-//in setview the 1nd parameter which is array is the latitude and longitude
-
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-// //the marker function also accepts 2 parameters that are latitude and longitude
-// L.marker([20.9320, 77.7523]).addTo(map)
-//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//     .openPopup();
